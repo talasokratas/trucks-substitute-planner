083f003b21b240dd59d2231d1653b0efbbf1882b
Evaldas Vaitonis <evaldas@rinkodara.lt>
2024-10-16 11:16:01 +0300

    truck model and relations

--- app/Models/Truck.php
+++ app/Models/Truck.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Truck extends Model
 {
-    //
+    protected $fillable = ['unit_number', 'year', 'notes'];
+    // A truck can have many subunits
+    public function subunits()
+    {
+        return $this->belongsToMany(Truck::class, 'truck_subunits', 'main_truck', 'subunit')
+            ->withPivot('start_date', 'end_date')
+            ->withTimestamps();
+    }
+
+    // A truck can also be a subunit to many main trucks
+    public function mainTrucks()
+    {
+        return $this->belongsToMany(Truck::class, 'truck_subunits', 'subunit', 'main_truck')
+            ->withPivot('start_date', 'end_date')
+            ->withTimestamps();
+    }
 }
